02b50f158eeedd888aa079a3589894f2693fdda0
Jonas de Araújo Luz Junior <git@jonasluz.com>
2016-09-17 09:05:58 -0300

    POC: Projeto Unity. Listagem das instâncias operacional.

---
+++ AWSManager/Assets/Code/AwsEnv.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Util;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+
+public class AwsEnv : MonoBehaviour {
+
+    // Singleton
+    AwsEnv instance;
+
+    public static AWSCredentials Credentials
+    {
+        get; private set;
+    }
+    public static AmazonEC2Config Config
+    {
+        get; private set;
+    }
+
+    void Awake()
+    {
+        // Implementa Singleton.
+        if (instance == null) instance = this;
+        else Destroy(this);
+    }
+
+	void Start ()
+    {
+        // Evitar erro de certificado no Mono.NET
+        ServicePointManager.ServerCertificateValidationCallback = MonoCertificateFix.MyRemoteCertificateValidationCallback;
+
+        // Ler o profile a partir de arquivo de configuração externo.
+        ReadProfileData();
+
+        // Define a configuração do endpoint.
+        Config = new AmazonEC2Config();
+        Config.RegionEndpoint = Amazon.RegionEndpoint.USEast1;
+    }
+
+    void ReadProfileData()
+    {
+        // Ler as credenciais do arquivo de configuração externo.
+        TextAsset data = Resources.Load<TextAsset>("credentials");
+        List<string> cfg = new List<string>(3);
+        foreach (string line in data.text.Split())
+            if (!line.Equals("")) cfg.Add(line);
+
+        // Registra o profile.
+        ProfileManager.RegisterProfile(cfg[0], cfg[1], cfg[2]);
+        // Recupera as credenciais.
+        Credentials = ProfileManager.GetAWSCredentials(cfg[0]);
+    }
+
+}
---
+++ AWSManager/Assets/Code/AwsTest.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Configuration;
+using System.Collections;
+
+using Amazon.Runtime;
+using Amazon.Util;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+
+public class AwsTest : MonoBehaviour
+{
+    void Start()
+    {
+        GetServiceOutput();
+    }
+
+    private void GetServiceOutput()
+    {
+        Debug.Log("===========================================");
+        Debug.Log("Welcome to the AWS .NET SDK!");
+        Debug.Log("===========================================");
+
+        // Print the number of Amazon EC2 instances.
+        IAmazonEC2 ec2 = new AmazonEC2Client(AwsEnv.Credentials, AwsEnv.Config);
+        try
+        {
+            DescribeInstancesRequest ec2Request = new DescribeInstancesRequest();
+            DescribeInstancesResponse ec2Response = ec2.DescribeInstances(ec2Request);
+            int numInstances = 0;
+            numInstances = ec2Response.Reservations.Count;
+            /*
+            Debug.Log(string.Format("You have {0} Amazon EC2 instance(s) running in the {1} region.",
+                                        numInstances, ConfigurationManager.AppSettings["AWSRegion"]));
+            */
+            foreach(Reservation ec2Reservation in ec2Response.Reservations)
+                foreach (Instance ec2Instance in ec2Reservation.Instances)
+                {
+                    Debug.Log(ec2Instance.KeyName);
+                }
+        }
+        catch (AmazonEC2Exception ex)
+        {
+            if (ex.ErrorCode != null && ex.ErrorCode.Equals("AuthFailure"))
+            {
+                Debug.Log("The account you are using is not signed up for Amazon EC2.");
+                Debug.Log("You can sign up for Amazon EC2 at http://aws.amazon.com/ec2");
+            }
+            else
+            {
+                Debug.Log("Caught Exception: " + ex.Message);
+                Debug.Log("Response Status Code: " + ex.StatusCode);
+                Debug.Log("Error Code: " + ex.ErrorCode);
+                Debug.Log("Error Type: " + ex.ErrorType);
+                Debug.Log("Request ID: " + ex.RequestId);
+            }
+        }
+    }
+}
