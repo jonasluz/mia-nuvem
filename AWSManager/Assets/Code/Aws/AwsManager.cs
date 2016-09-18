475ac71106139360062ccc97dfc05d07e296c330
Jonas de Araújo Luz Junior <git@jonasluz.com>
2016-09-18 09:02:41 -0300

    AwsManager : Completando a implementação dos métodos solicitados no trabalho.

--- AWSManager/Assets/Code/Aws/AwsManager.cs
+++ AWSManager/Assets/Code/Aws/AwsManager.cs
@@ -37,7 +37,7 @@ public class AwsManager : MonoBehaviour {
     /// Recupera as instâncias EC2 existentes.
     /// </summary>
     /// <returns>Lista de instâncias EC2</returns>
-    public IEnumerable<Instance> GetInstances()
+    public IList<Instance> GetInstances()
     {
         List<Instance> result = new List<Instance>();
 
@@ -58,7 +58,30 @@ public class AwsManager : MonoBehaviour {
             throw new AwsException(ex);
         }
 
-        return result.ToArray();
+        return result;
+    }
+
+    public IList<Image> GetImages()
+    {
+        IList<Image> result = new List<Image>();
+
+        // Recupera as imagens.
+        try
+        {
+            DescribeImagesRequest ec2Request = new DescribeImagesRequest();
+            DescribeImagesResponse ec2Response = Ec2.DescribeImages(ec2Request);
+
+            foreach (Image ec2Image in ec2Response.Images)
+            {
+                result.Add(ec2Image);
+            }
+        }
+        catch (AmazonEC2Exception ex)
+        {
+            throw new AwsException(ex);
+        }
+
+        return result;
     }
 
     public IDictionary<string, string> RunInstance()
@@ -76,6 +99,8 @@ public class AwsManager : MonoBehaviour {
             Debug.Log(runRequest);
             RunInstancesResponse runResponse = Ec2.RunInstances(runRequest);
             result = runResponse.ResponseMetadata.Metadata;
+            result.Add("STATUS_CODE", runResponse.HttpStatusCode.ToString());
+            result.Add("RESPONSE", runResponse.ToString());
         }
         catch (AmazonEC2Exception ex)
         {
@@ -84,4 +109,80 @@ public class AwsManager : MonoBehaviour {
 
         return result;
     }
+
+    public IDictionary<string, string> StartInstance(string instanceId)
+    {
+        return StartInstance(new List<string>(){ instanceId });
+    }
+    public IDictionary<string, string> StartInstance(List<string> instancesIds)
+    {
+        IDictionary<string, string> result;
+
+        // Inicia uma instância.
+        try
+        {
+            StartInstancesRequest startRequest = new StartInstancesRequest(instancesIds);
+            StartInstancesResponse startResponse = Ec2.StartInstances(startRequest);
+            result = startResponse.ResponseMetadata.Metadata;
+            result.Add("STATUS_CODE", startResponse.HttpStatusCode.ToString());
+            result.Add("RESPONSE", startResponse.ToString());
+        }
+        catch (AmazonEC2Exception ex)
+        {
+            throw new AwsException(ex);
+        }
+
+        return result;
+    }
+
+    public IDictionary<string, string> StopInstance(string instanceId)
+    {
+        return StopInstance(new List<string>() { instanceId });
+    }
+    public IDictionary<string, string> StopInstance(List<string> instancesIds)
+    {
+        IDictionary<string, string> result;
+
+        // Para uma instância.
+        try
+        {
+            StopInstancesRequest stopRequest = new StopInstancesRequest(instancesIds);
+            StopInstancesResponse stopResponse = Ec2.StopInstances(stopRequest);
+            result = stopResponse.ResponseMetadata.Metadata;
+            result.Add("STATUS_CODE", stopResponse.HttpStatusCode.ToString());
+            result.Add("RESPONSE", stopResponse.ToString());
+        }
+        catch (AmazonEC2Exception ex)
+        {
+            throw new AwsException(ex);
+        }
+
+        return result;
+    }
+
+    public IDictionary<string, string> TerminateInstance(string instanceId)
+    {
+        return TerminateInstance(new List<string>() { instanceId });
+    }
+    public IDictionary<string, string> TerminateInstance(List<string> instancesIds)
+    {
+        IDictionary<string, string> result;
+
+        // Destrói uma instância.
+        try
+        {
+            TerminateInstancesRequest terminateRequest = new TerminateInstancesRequest(instancesIds);
+            TerminateInstancesResponse terminateResponse = Ec2.TerminateInstances(terminateRequest);
+            result = terminateResponse.ResponseMetadata.Metadata;
+            result.Add("STATUS_CODE", terminateResponse.HttpStatusCode.ToString());
+            result.Add("RESPONSE", terminateResponse.ToString());
+        }
+        catch (AmazonEC2Exception ex)
+        {
+            throw new AwsException(ex);
+        }
+
+        return result;
+    }
+
 }
