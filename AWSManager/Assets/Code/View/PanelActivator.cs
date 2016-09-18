ff57a575da504ba817bda320d5578bfb3d71c095
Jonas de Araújo Luz Junior <git@jonasluz.com>
2016-09-18 17:44:22 -0300

    Interface 3D: Animação do painel principal. Botões de nova instância e atualização da lista.

---
+++ AWSManager/Assets/Code/View/PanelActivator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PanelActivator : AbstractButtonSphere {
+
+    public GameObject panel;
+
+    private bool cancelClosing = false;
+
+    public override void Execute()
+    {
+        if (panel)
+        {
+            Animator animtr = panel.GetComponent<Animator>();
+
+            if (panel.activeSelf)
+            {
+                cancelClosing = false;
+                if (animtr) animtr.SetTrigger("Hide");
+                StartCoroutine("WaitAnimationAndInactive", animtr);
+            }
+            else
+            {
+                cancelClosing = true;
+                panel.SetActive(true);
+                if (animtr) animtr.SetTrigger("Show");
+            }
+        }
+    }
+
+    IEnumerator WaitAnimationAndInactive(Animator animtr)
+    {
+        float length = animtr.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(length);
+        if (!cancelClosing) panel.SetActive(false);
+    }
+}
