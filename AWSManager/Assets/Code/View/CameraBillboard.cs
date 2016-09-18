d354e8f73fd2e136648ff13126044418072b1850
Jonas de Araújo Luz Junior <git@jonasluz.com>
2016-09-18 15:52:40 -0300

    Standard Assets - Camera. LookatTarget, usado por CameraBillboard. Usados para que a interface das nuvens esteja sempre de frente para a câmera.

---
+++ AWSManager/Assets/Code/View/CameraBillboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+using UnityStandardAssets.Cameras;
+
+[RequireComponent(typeof(UnityStandardAssets.Cameras.LookatTarget))]
+public class CameraBillboard : MonoBehaviour {
+
+    private LookatTarget m_lookAt; 
+
+    void Awake()
+    {
+        m_lookAt = GetComponentInChildren<LookatTarget>();
+        m_lookAt.SetTarget(Camera.main.transform);
+    }
+	
+}
