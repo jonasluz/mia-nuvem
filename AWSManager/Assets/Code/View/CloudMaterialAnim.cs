5a2ab99f6b2499a512f00bc5505699cd7ae2b51e
Jonas de Araújo Luz Junior <git@jonasluz.com>
2016-09-19 07:13:07 -0300

    Material Procedural / Substance: Nuvens. Com script de animação.

---
+++ AWSManager/Assets/Code/View/CloudMaterialAnim.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMaterialAnim : MonoBehaviour {
+
+    [Range(0,1)] public float animSpeed = .1f;
+
+    ProceduralMaterial substance;
+
+    void Awake()
+    {
+        substance = gameObject.GetComponent<MeshRenderer>().sharedMaterial as ProceduralMaterial;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if (substance)
+        {
+            float offset = substance.GetProceduralFloat("offset") + animSpeed;
+            if (offset > .5) offset = -.5f;
+            substance.SetProceduralFloat("offset", offset);
+            substance.RebuildTextures();
+        }
+	}
+}
