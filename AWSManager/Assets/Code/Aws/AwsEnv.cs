26bbff7d73ebd106539c1d5aefaa92d675705381
Jonas de Araújo Luz Junior <git@jonasluz.com>
2016-09-21 07:20:53 -0300

    Configuração: Parâmetro do KEY_NAME adicionado no arquivo de configuração (Resources/credentials.txt). Permite que o programa seja facilmente configurado para uso com outras contas AWS.

--- AWSManager/Assets/Code/Aws/AwsEnv.cs
+++ AWSManager/Assets/Code/Aws/AwsEnv.cs
@@ -17,6 +17,15 @@ public class AwsEnv : MonoBehaviour {
     public static readonly string DEFAULT_INST_TYPE = "t2.micro";
     public static readonly string DEFAULT_KEY_NAME = "JALJ_AWS_MIA-1";
     public static readonly string DEFAULT_SECURITY_GROUP = "default";
+    
+    public static string KeyName
+    {
+        get
+        {
+            return s_keyname == null ? DEFAULT_KEY_NAME : s_keyname;
+        }
+    }
+    private static string s_keyname; 
 
     public static AWSCredentials Credentials
     {
@@ -52,11 +61,12 @@ public class AwsEnv : MonoBehaviour {
     {
         // Ler as credenciais do arquivo de configuração externo.
         TextAsset data = Resources.Load<TextAsset>("credentials");
-        List<string> cfg = new List<string>(3);
+        List<string> cfg = new List<string>(4);
         foreach (string line in data.text.Split())
             if (!line.Equals("")) cfg.Add(line);
 
         // Registra o profile.
+        s_keyname = cfg.Count == 4 ? cfg[3] : null;
         ProfileManager.RegisterProfile(cfg[0], cfg[1], cfg[2]);
         // Recupera as credenciais.
         Credentials = ProfileManager.GetAWSCredentials(cfg[0]);
